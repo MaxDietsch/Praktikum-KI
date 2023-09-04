cd72b669e5f3b64c33baaf20d4cded36ba775f15
Max Dietsch <diedietschies@gmail.com>
2023-09-04 13:08:09 +0200

    train-test

--- mmselfsup/datasets/kvasir.py
+++ mmselfsup/datasets/kvasir.py
@@ -2,7 +2,8 @@ from typing import List, Optional, Union
 from mmcls.datasets import CustomDataset
 from mmselfsup.registry import DATASETS
 from mmcls.datasets import BaseDataset
-
+from PIL import Image
+import torchvision.transforms as transform
 
 @DATASETS.register_module()
 class Kvasir(BaseDataset):
@@ -26,12 +27,20 @@ class Kvasir(BaseDataset):
     def load_data_list(self) -> List[dict]:
         assert isinstance(self.ann_file, str)
         data_list = []
+
+        transform = transforms.Compose([transforms.ToTensor()])
+
         with open(self.ann_file) as f:
             samples = [x.strip().split(' ') for x in f.readlines()]
             for filename, gt_label in samples:
                 #img_path = add_prefix(filename, self.img_prefix)
                 img_path = self.img_prefix + "/" + filename
                 info = {'img_path': img_path, 'gt_label': int(gt_label)}
+
+                img = Image.open(img_path)
+                inputs = transform(img)
+                info = {'inputs': inputs, 'gt_label': int(gt_label)}
+
                 data_list.append(info)
         print(data_list)
         return data_list
