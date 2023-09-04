1ecbb96b8ba2e02eac0e679fe83da90376f64745
Max Dietsch <diedietschies@gmail.com>
2023-09-04 13:15:55 +0200

    train-test

--- mmselfsup/datasets/kvasir.py
+++ mmselfsup/datasets/kvasir.py
@@ -1,12 +1,10 @@
 from typing import List, Optional, Union
 from mmcls.datasets import CustomDataset
 from mmselfsup.registry import DATASETS
-from mmcls.datasets import BaseDataset
-from PIL import Image
-import torchvision.transforms as transforms
+from mmengine.fileio import join_path
 
 @DATASETS.register_module()
-class Kvasir(BaseDataset):
+class Kvasir(CustomDataset):
 
     IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif')
 
@@ -16,32 +14,28 @@ class Kvasir(BaseDataset):
                  data_root: str = '',
                  data_prefix: Union[str, dict] = '',
                  **kwargs) -> None:
-        #kwargs = {'extensions': self.IMG_EXTENSIONS, **kwargs}
+        kwargs = {'extensions': self.IMG_EXTENSIONS, **kwargs}
         super().__init__(
             ann_file=ann_file,
             metainfo=metainfo,
             data_root=data_root,
-            data_prefix=data_prefix)
-            #**kwargs)
+            data_prefix=data_prefix,
+            **kwargs)
 
     def load_data_list(self) -> List[dict]:
-        assert isinstance(self.ann_file, str)
+        assert self.ann_file != ''
+        with open(self.ann_file, 'r') as f:
+            self.samples = f.readlines()
+        self.has_labels = len(self.samples[0].split()) == 2
+
         data_list = []
-
-        transform = transforms.Compose([transforms.ToTensor()])
-
-        with open(self.ann_file) as f:
-            samples = [x.strip().split(' ') for x in f.readlines()]
-            for filename, gt_label in samples:
-                #img_path = add_prefix(filename, self.img_prefix)
-                img_path = self.img_prefix + "/" + filename
-                info = {'img_path': img_path, 'gt_label': int(gt_label)}
-
-                img = Image.open(img_path)
-                inputs = transform(img)
-                info = {'inputs': inputs, 'gt_label': int(gt_label)}
-
-                data_list.append(info)
-        print(data_list)
+        for sample in self.samples:
+            info = {'img_prefix': self.img_prefix}
+            sample = sample.split()
+            info['img_path'] = join_path(self.img_prefix, sample[0])
+            info['img_info'] = {'filename': sample[0]}
+            labels = sample[1] if self.has_labels else -1
+            info['gt_label'] = np.array(labels, dtype=np.int64)
+            data_list.append(info)
         return data_list
 
