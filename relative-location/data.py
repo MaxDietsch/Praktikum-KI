f6f21eeea8e8fa020737964b1e481c644f8d9bb9
Max Dietsch <diedietschies@gmail.com>
2023-09-04 13:24:40 +0200

    train-test

--- relative-location/data.py
+++ relative-location/data.py
@@ -19,7 +19,7 @@ train_pipeline = [
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='ResizeEdge', scale=256, edge='short', backend='pillow'),
+    dict(type='mmcls.ResizeEdge', scale=256, edge='short', backend='pillow'),
     dict(type='CenterCrop', crop_size=224),
     dict(type='PackSelfSupInputs'),
 ]
