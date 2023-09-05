cbc7348fcafd0289963b8154eeccc9bad6c19c7e
Max Dietsch <diedietschies@gmail.com>
2023-09-05 13:41:11 +0200

    train-test

--- mae/pretrain_data.py
+++ mae/pretrain_data.py
@@ -15,7 +15,7 @@ train_pipeline = [
 ]
 
 train_dataloader = dict(
-    batch_size=128,
+    batch_size=64,
     num_workers=8,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
