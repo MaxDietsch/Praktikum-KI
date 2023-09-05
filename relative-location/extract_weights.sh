84d16004b9fdacb7e65c48809fe939ec444688c1
Max Dietsch <diedietschies@gmail.com>
2023-09-05 10:28:36 +0200

    train-test

--- relative-location/extract_weights.sh
+++ relative-location/extract_weights.sh
@@ -1,3 +1,3 @@
 python ../tools/model_converters/extract_backbone_weights.py \
-  work_dirs/selfsup/resnet50/epoch_30.pth \
+  work_dirs/selfsup/resnet50/epoch_50.pth \
   work_dirs/selfsup/resnet50/resnet_backbone-weights.pth
