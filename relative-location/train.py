2761e17b2a506161f1d46f0eb3979e3aea46368b
Max Dietsch <diedietschies@gmail.com>
2023-09-05 07:58:26 +0200

    train-test

--- relative-location/train.py
+++ relative-location/train.py
@@ -7,7 +7,7 @@ benchmark_cfg = Config.fromfile('configs.py')
 
 checkpoint_file = './work_dirs/selfsup/relative-loc_resnet50_8xb64-steplr-70e_in1k_colab/relative-loc_backbone-weights.pth'
 
-benchmark_cfg.model.backbone.frozen_stages=4
+benchmark_cfg.model.backbone.frozen_stages=0
 benchmark_cfg.model.backbone.init_cfg = dict(type='Pretrained', checkpoint=checkpoint_file)
 
 benchmark_cfg.work_dir = './work_dirs/benchmarks/classification/imagenet/resnet50_8xb32-steplr-100e_in1k_colab'
