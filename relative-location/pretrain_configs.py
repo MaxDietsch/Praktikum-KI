6a5511c55e3d92c94158cfdf127653e6c282a1eb
Max Dietsch <diedietschies@gmail.com>
2023-09-05 09:39:45 +0200

    train-test

--- relative-location/pretrain_configs.py
+++ relative-location/pretrain_configs.py
@@ -5,22 +5,7 @@ _base_ = [
     './runtime.py'
 ]
 
-
 default_hooks = dict(logger=dict(type='LoggerHook', interval=10))
-
-optimizer = dict(type='SGD', lr=0.2, momentum=0.9, weight_decay=1e-4)
-optim_wrapper = dict(
-    type='OptimWrapper',
-    optimizer=optimizer,
-    paramwise_cfg=dict(custom_keys={
-        'neck': dict(decay_mult=5.0),
-        'head': dict(decay_mult=5.0)
-    }))
-
-param_scheduler = [dict(type='MultiStepLR', by_epoch=True, milestones=[10, 15, 23])]
-
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=30)
-
 default_hooks = dict(checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=3))
 
 work_dir = './work_dirs/selfsup/resnet50'
--- relative-location/pretrain_schedule.py
+++ relative-location/pretrain_schedule.py
@@ -1,11 +1,17 @@
 # optimizer wrapper
-optimizer = dict(type='SGD', lr=0.3, weight_decay=1e-4, momentum=0.9)
-optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer)
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=1e-4)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=optimizer,
+    paramwise_cfg=dict(custom_keys={
+        'neck': dict(decay_mult=5.0),
+        'head': dict(decay_mult=5.0)
+    }))
+
 
 # learning rate scheduler
-param_scheduler = [
-    dict(type='MultiStepLR', by_epoch=True, milestones=[3, 6], gamma=0.1)
-]
+param_scheduler = [dict(type='MultiStepLR', by_epoch=True, milestones=[15, 25, 35], gamma = 0.1)]
+
 
 # runtime settings
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=10)
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=50)
