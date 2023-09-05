acadf5b6cdfddc0e573f888b933d763f7b4b70b6
Max Dietsch <diedietschies@gmail.com>
2023-09-05 13:33:12 +0200

    train-test

--- mae/pretrain_schedule.py
+++ mae/pretrain_schedule.py
@@ -1,6 +1,17 @@
 # optimizer wrapper
-optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=1e-4)
-optim_wrapper = dict(type='AmpOptimWrapper', loss_scale='dynamic')
+optimizer = dict(
+    type='AdamW', lr=1.5e-4 * 4096 / 256, betas=(0.9, 0.95), weight_decay=0.05)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=optimizer,
+    paramwise_cfg=dict(
+        custom_keys={
+            'ln': dict(decay_mult=0.0),
+            'bias': dict(decay_mult=0.0),
+            'pos_embed': dict(decay_mult=0.),
+            'mask_token': dict(decay_mult=0.),
+            'cls_token': dict(decay_mult=0.)
+        }))
 
 
 
