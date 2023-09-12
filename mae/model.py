4d8d2430bf46aa85b5c15df0896d8b56ec7aa162
Max Dietsch <diedietschies@gmail.com>
2023-09-12 15:57:02 +0200

    test vit

--- mae/model.py
+++ mae/model.py
@@ -7,15 +7,23 @@ model = dict(
         std=[81.42, 61.04, 58.85],
         to_rgb=True
         ),
-    backbone=dict(type='mmcls.LinearVisionTransformer', arch='s', patch_size=16, frozen_stages = 8, init_cfg = 'Xavier'),
+    backbone=dict(
+        type='mmcls.LinearVisionTransformer', 
+        arch='s', 
+        patch_size=16, 
+        frozen_stages = 8, 
+        init_cfg = dict(type='Xavier')
+        ),
     #neck=dict(type='mmcls.GlobalAveragePooling'),
     head=dict(
         type='mmcls.LinearClsHead',
         num_classes=3,
         in_channels=768,
         loss=dict(type='mmcls.CrossEntropyLoss', loss_weight=1.0),
+        init_cfg = dict(type = 'Xavier'),
         topk=(1, 1)
-    ))
+        )
+    )
 
 """
     head=dict(
