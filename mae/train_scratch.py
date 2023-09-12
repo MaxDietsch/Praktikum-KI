4819141464e8f28f71ab9b6ce3278e2bd7af7e65
Max Dietsch <diedietschies@gmail.com>
2023-09-12 15:39:29 +0200

    test vit

--- mae/train_scratch.py
+++ mae/train_scratch.py
@@ -6,10 +6,9 @@ import os
 benchmark_cfg = Config.fromfile('configs.py')
 
 benchmark_cfg.work_dir = './work_dirs/train_scratch/vit_p16'
-#benchmark_cfg.resume = False
-#benchmark_cfg.load_from = None
-benchmark_cfg.model.backbone.init_cfg = None
-benchmark_cfg.model.head.init_cfg = None
+#benchmark_cfg.model.backbone.init_cfg = None
+#benchmark_cfg.model.head.init_cfg = None
+benchmark_cfg.model.init_cfg = dict(type='Xavier')
 
 
 benchmark_cfg.randomness = dict(seed=0, deterministic=True)
