f20efe668a107faf9685adc441d90cee26467acd
Max Dietsch <diedietschies@gmail.com>
2023-09-14 15:39:59 +0200

    test vit

--- mae/extract_weights.sh
+++ mae/extract_weights.sh
@@ -1,3 +1,3 @@
 python ../tools/model_converters/extract_backbone_weights.py \
-  work_dirs/selfsup/vit_p16/epoch_50.pth \
+  work_dirs/selfsup/vit_p16/epoch_200.pth \
   work_dirs/selfsup/vit_p16/vit_p16_backbone-weights.pth
