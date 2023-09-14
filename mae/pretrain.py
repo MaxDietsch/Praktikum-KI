3505af9116e24b53cd0346e02200587e90ea6db3
Max Dietsch <diedietschies@gmail.com>
2023-09-14 16:18:29 +0200

    test vit

--- mae/pretrain.py
+++ mae/pretrain.py
@@ -7,6 +7,8 @@ if torch.cuda.is_available():
     os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':16:8'
 
 cfg = Config.fromfile("./pretrain_configs.py")
+cfg.resume = True
+cfg.load_from = './work_dirs/selfsup/vit_p16/epoch_200.pth'
 
 runner = Runner.from_cfg(cfg)
 runner.train()
