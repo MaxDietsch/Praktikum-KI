cc1acf342747125dc6ec3ca0a4b0afa33f1ca7d8
Max Dietsch <diedietschies@gmail.com>
2023-09-19 13:34:07 +0200

    test

--- tools_created/unzip.py
+++ tools_created/unzip.py
@@ -1,8 +1,10 @@
 import zipfile
 
-def unzip_file(zip_filepath, dest_folder):
+def unzip_file(zip_filepath, dest_folder, password=None):
     with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
+        if password:
+            zip_ref.setpassword(password.encode())
         zip_ref.extractall(dest_folder)
 
 # Usage:
-unzip_file('../../SUN/sundatabase_negative_part1.zip', './download_unzip/SUN/negative')
+unzip_file('../../SUN/sundatabase_negative_part1.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
