7d03c76f28595d187f0c3412bfb5984203840dfb
Max Dietsch <diedietschies@gmail.com>
2023-09-19 14:15:36 +0200

    test

--- tools_created/unzip.py
+++ tools_created/unzip.py
@@ -7,4 +7,20 @@ def unzip_file(zip_filepath, dest_folder, password=None):
         zip_ref.extractall(dest_folder)
 
 # Usage:
-unzip_file('../../SUN/sundatabase_negative_part1.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
+print("part 1")
+unzip_file('../../SUN/sundatabase_negative_part2.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
+
+print("part 2")
+unzip_file('../../SUN/sundatabase_negative_part2.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
+
+print("part 3")
+unzip_file('../../SUN/sundatabase_negative_part3.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
+
+print("part 4")
+unzip_file('../../SUN/sundatabase_negative_part4.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
+
+print("part 5")
+unzip_file('../../SUN/sundatabase_positive_part1.zip', './download_unzip/SUN/positive', 'pS2oU0lN2yP0')
+
+print("part 6")
+unzip_file('../../SUN/sundatabase_positive_part2.zip', './download_unzip/SUN/positive', 'pS2oU0lN2yP0')
