d948d260c4f541ba001cc0071f04c4a8954a16cc
Max Dietsch <diedietschies@gmail.com>
2023-09-19 09:44:33 +0200

    test

--- mae/data.py
+++ mae/data.py
@@ -4,7 +4,7 @@ data_root = '../data_dir/kvasir/'
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', interpolation='bicubic'),
+    dict(type='Resize', scale_factor=0.5, interpolation='bicubic'),
     dict(type='RandomFlip', prob=0.5),
     dict(type='mmcls.PackClsInputs'),
 ]
