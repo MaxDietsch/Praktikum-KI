76a2bfee3d0eaa002b92843871635f80c520817f
Max Dietsch <diedietschies@gmail.com>
2023-09-20 20:03:14 +0200

    unzip

--- tools_created/unzip.py
+++ tools_created/unzip.py
@@ -7,21 +7,27 @@ def unzip_file(zip_filepath, dest_folder, password=None):
         zip_ref.extractall(dest_folder)
 
 # Usage:
-print("part 1")
+print("part 3")
 unzip_file('../../SUN/sundatabase_negative_part1.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
 
-print("part 2")
+print("part 4")
 unzip_file('../../SUN/sundatabase_negative_part2.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
 
-print("part 3")
+print("part 5")
 unzip_file('../../SUN/sundatabase_negative_part3.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
 
-print("part 4")
+print("part 6")
 unzip_file('../../SUN/sundatabase_negative_part4.zip', './download_unzip/SUN/negative', 'pS2oU0lN2yP0')
 
-print("part 5")
+print("part 7")
 unzip_file('../../SUN/sundatabase_positive_part1.zip', './download_unzip/SUN/positive', 'pS2oU0lN2yP0')
 
-print("part 6")
+print("part 8")
 unzip_file('../../SUN/sundatabase_positive_part2.zip', './download_unzip/SUN/positive', 'pS2oU0lN2yP0')
+
+print("part 1")
+unzip_file('../../LD/videos with polyps.zip', './download_unzip/LD/positive')
+
+print("part 2")
+unzip_file('../../LD/videos without polyps.zip', './download_unzip/LD/negative')
 print("finished")
