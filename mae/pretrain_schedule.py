95221eb80bd1804004921552b5428472dcc5693c
Max Dietsch <diedietschies@gmail.com>
2023-09-21 16:07:03 +0200

    unzip

--- mae/pretrain_schedule.py
+++ mae/pretrain_schedule.py
@@ -1,6 +1,21 @@
 # optimizer wrapper
+"""
 optimizer = dict(
     type='AdamW', lr=1.5e-3 * 4096 / 256, betas=(0.9, 0.95), weight_decay=0.05)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=optimizer,
+    paramwise_cfg=dict(
+        custom_keys={
+            'ln': dict(decay_mult=0.0),
+            'bias': dict(decay_mult=0.0),
+            'pos_embed': dict(decay_mult=0.),
+            'mask_token': dict(decay_mult=0.),
+            'cls_token': dict(decay_mult=0.)
+        }))
+"""
+
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optim_wrapper = dict(
     type='OptimWrapper',
     optimizer=optimizer,
