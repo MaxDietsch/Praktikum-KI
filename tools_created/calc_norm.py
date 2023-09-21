6c297fd70d333b24c33e19bccd0caaff74d23922
Max Dietsch <diedietschies@gmail.com>
2023-09-21 16:51:52 +0200

    unzip

--- tools_created/calc_norm.py
+++ tools_created/calc_norm.py
@@ -4,7 +4,10 @@ import os
 def calc_norm(dir):
     sum_r, sum_g, sum_b = 0.0, 0.0, 0.0
     sum_sq_diff_r, sum_sq_diff_g, sum_sq_diff_b = 0.0, 0.0, 0.0
+    i = 0
     for img_name in os.listdir(dir):
+        print(i)
+        i += 1
         # Ensure the file is an image
         if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
             continue
@@ -16,7 +19,10 @@ def calc_norm(dir):
 
         sum_r += image[:, :, 0].mean()
         sum_g += image[:, :, 1].mean()
-        sum_b += image[:, :, 2].mean()        
+        sum_b += image[:, :, 2].mean()   
+
+        if i >= 10000:
+            break     
 
     
     num_images = len(os.listdir(dir))
@@ -24,8 +30,10 @@ def calc_norm(dir):
     mean_g = sum_g / num_images
     mean_b = sum_b / num_images
 
-
+    i = 0
     for img_name in os.listdir(dir):
+        print(i)
+        i += 1
         # Ensure the file is an image
         if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
             continue
@@ -39,6 +47,9 @@ def calc_norm(dir):
         sum_sq_diff_g += ((image[:, :, 1] - mean_g) ** 2).mean()
         sum_sq_diff_b += ((image[:, :, 2] - mean_b) ** 2).mean()
 
+        if i >= 10000:
+            break
+
     var_r = sum_sq_diff_r / num_images
     var_g = sum_sq_diff_g / num_images
     var_b = sum_sq_diff_b / num_images
@@ -51,5 +62,5 @@ def calc_norm(dir):
     print(f"standard deviations: r: {std_r}, g: {std_g}, b: {std_b}")
 
 
-directory = './data_dir/kvasir/train'
+directory = './data_dir/LD/train'
 calc_norm(directory)
