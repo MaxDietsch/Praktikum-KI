f64d6c546215f4c391eb189149b5e12ca55a146e
Max Dietsch <diedietschies@gmail.com>
2023-09-21 07:45:45 +0200

    unzip

--- tools_created/copy.py
+++ tools_created/copy.py
@@ -4,8 +4,8 @@ import shutil
 def copy_all_files(src_dir, dest_dir):
     # Iterate through all files in the source directory
     for dir in os.listdir(src_dir):
-        for filename in os.listdir(dir):
-            src_file_path = os.path.join(src_dir, dir, filename)
+        for filename in os.listdir(os.path.join(src_dir, dir)):
+            src_file_path = os.path.join(src_dir, filename)
             dest_file_path = os.path.join(dest_dir, filename)
 
         # Check if it's a file and not a directory
