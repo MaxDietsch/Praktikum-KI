94cdb8fc3ed5aee10a1090fbfccd2c2d1ba21c20
Max Dietsch <diedietschies@gmail.com>
2023-09-22 17:55:07 +0200

    unzip

--- simsiam/pretrain_data.py
+++ simsiam/pretrain_data.py
@@ -4,23 +4,7 @@ data_root = '../data_dir/LD'
 
 view_pipeline = [
     dict(type='Resize', scale=(640, 640), interpolation='bicubic'),
-    dict(
-        type='RandomApply',
-        transforms=[
-            dict(
-                type='ColorJitter',
-                brightness=0.4,
-                contrast=0.4,
-                saturation=0.4,
-                hue=0.1)
-        ],
-        prob=0.8),
-    dict(
-        type='RandomGrayscale',
-        prob=0.2,
-        keep_channels=True,
-        channel_weights=(0.114, 0.587, 0.2989)),
-    dict(type='RandomGaussianBlur', sigma_min=0.1, sigma_max=2.0, prob=0.5),
+    dict(type='RandomGaussianBlur', sigma_min=0.1, sigma_max=2.0, prob=0.3),
     dict(type='RandomFlip', prob=0.5),
 ]
 
@@ -31,7 +15,7 @@ train_pipeline = [
 ]
 
 train_dataloader = dict(
-    batch_size=32,
+    batch_size=16,
     num_workers=8,
     drop_last=True,
     persistent_workers=True,
