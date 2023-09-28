b52b88d6ce01b0a3a906b186a3ac0005fac3862b
Max Dietsch <diedietschies@gmail.com>
2023-09-28 19:04:58 +0200

    20

--- relative-location/schedule.py
+++ relative-location/schedule.py
@@ -1,5 +1,5 @@
 # optimizer wrapper
-optimizer = dict(type='SGD', lr=0.0001, weight_decay=1e-4, momentum=0.9)
+optimizer = dict(type='SGD', lr=0.00001, weight_decay=1e-4, momentum=0.9)
 optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer)
 
 # learning rate scheduler
