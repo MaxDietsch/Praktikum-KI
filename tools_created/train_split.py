101f44e9c68c166a8256601996eb2d5f71e99170
Max Dietsch <diedietschies@gmail.com>
2023-09-29 21:19:38 +0200

    20

--- tools_created/train_split.py
+++ tools_created/train_split.py
@@ -3,6 +3,17 @@ import random
 import shutil
 import tqdm
 
+
+def check_ratio(filename, size):
+    idx = 0
+    with open(filename, "r") as f:
+        for line in f:
+            file, cls = line.split()
+            if cls == 0:
+                idx += 1
+    return idx / size
+
+
 def create_split(directory, percentage):
     filename = os.path.join(directory, 'meta/train.txt')
 
@@ -12,9 +23,9 @@ def create_split(directory, percentage):
     os.mkdir(directory + f'/train{str(percentage * 100)}%')
     """
 
-    if os.path.exists(directory + f'/train5000'):
-        shutil.rmtree(directory + f'/train5000')
-    os.mkdir(directory + f'/train5000')
+    if os.path.exists(directory + f'/train1000'):
+        shutil.rmtree(directory + f'/train1000')
+    os.mkdir(directory + f'/train1000')
 
 
     lines = []
@@ -24,21 +35,25 @@ def create_split(directory, percentage):
                 lines.append(line.strip())
     
     #train_file = os.path.join(directory, 'meta', f'train{str(percentage * 100)}%.txt')
-    train_file = os.path.join(directory, 'meta', f'train5000.txt')
+    train_file = os.path.join(directory, 'meta', f'train1000.txt')
 
 
     with open(train_file, "w") as trainfile:
         for i, line in tqdm.tqdm(enumerate(lines)):
             file, cls = line.split()
             #shutil.copy(os.path.join(directory, 'train', file), directory + f'/train{str(percentage * 100)}%')
-            shutil.copy(os.path.join(directory, 'train', file), directory + f'/train5000')
+            shutil.copy(os.path.join(directory, 'train', file), directory + f'/train1000')
             trainfile.write(line + '\n')
 
 
 if __name__ == "__main__":
     directory = "../data_dir/SUN"
-    percentage = 5000/63490 #0.25
+    percentage = 1000/63490 #0.25
     create_split(directory, percentage)
+
+    x = check_ratio("../data_dir/SUN/meta/train1000.txt", 1000)
+    print(f"ratio of positive files is: {x}")
+
 
 
     
