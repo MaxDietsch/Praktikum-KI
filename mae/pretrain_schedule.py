c41e4a0e5b7347c9be473e30b0b0566cdcd1e196
Max Dietsch <diedietschies@gmail.com>
2023-10-02 19:46:09 +0200

    simsiam

--- mae/pretrain_schedule.py
+++ mae/pretrain_schedule.py
@@ -1,51 +1,16 @@
 # optimizer wrapper
-"""
-optimizer = dict(
-    type='AdamW', lr=1.5e-3 * 4096 / 256, betas=(0.9, 0.95), weight_decay=0.05)
+optimizer = dict(type='SGD', lr=0.007, momentum=0.9, weight_decay=1e-4)
 optim_wrapper = dict(
     type='OptimWrapper',
     optimizer=optimizer,
-    paramwise_cfg=dict(
-        custom_keys={
-            'ln': dict(decay_mult=0.0),
-            'bias': dict(decay_mult=0.0),
-            'pos_embed': dict(decay_mult=0.),
-            'mask_token': dict(decay_mult=0.),
-            'cls_token': dict(decay_mult=0.)
-        }))
-"""
+    paramwise_cfg=dict(custom_keys={
+        'neck': dict(decay_mult=5.0),
+        'head': dict(decay_mult=5.0)
+    }))
 
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
-optim_wrapper = dict(
-    type='OptimWrapper',
-    optimizer=optimizer,
-    paramwise_cfg=dict(
-        custom_keys={
-            'ln': dict(decay_mult=0.0),
-            'bias': dict(decay_mult=0.0),
-            'pos_embed': dict(decay_mult=0.),
-            'mask_token': dict(decay_mult=0.),
-            'cls_token': dict(decay_mult=0.)
-        }))
 
 # learning rate scheduler
-param_scheduler = [
-    dict(
-        type='LinearLR',
-        start_factor=0.0001,
-        by_epoch=True,
-        begin=0,
-        end=100,
-        convert_to_iter_based=True),
-    dict(
-        type='CosineAnnealingLR',
-        T_max=260,
-        by_epoch=True,
-        begin=200,
-        end=400,
-        convert_to_iter_based=True)
-]
-
+param_scheduler = [dict(type='MultiStepLR', by_epoch=True, milestones=[15, 25, 35], gamma = 0.1)]
 
 
 # runtime settings
