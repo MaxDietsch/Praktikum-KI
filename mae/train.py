0186e3877b3bb92a4e3d801380e8747769f8f751
Max Dietsch <diedietschies@gmail.com>
2023-10-03 19:11:56 +0200

    mae

--- mae/train.py
+++ mae/train.py
@@ -6,7 +6,7 @@ import os
 cfg = Config.fromfile('configs.py')
 checkpoint_file = './work_dirs/pretrain/vit_p16_backbone-weights.pth'
 cfg.model.backbone.init_cfg = dict(type='Pretrained', checkpoint=checkpoint_file)
-cfg.work_dir = './work_dirs/train/100%007'
+cfg.work_dir = './work_dirs/train/100%'
 
 runner = Runner.from_cfg(cfg)
 runner.train()
