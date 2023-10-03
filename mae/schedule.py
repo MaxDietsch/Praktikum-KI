d224626190eb3b0c3ec500189a116f525edb4472
Max Dietsch <diedietschies@gmail.com>
2023-10-03 18:46:23 +0200

    mae

--- mae/schedule.py
+++ mae/schedule.py
@@ -1,21 +1,4 @@
 
-"""
-# optimizer wrapper
-optimizer = dict(
-    type='AdamW', lr=1.5e-3 * 4096 / 256, betas=(0.9, 0.95), weight_decay=0.0)
-    #type='AdamW', lr=0, betas=(0.9, 0.95), weight_decay=0.05)
-optim_wrapper = dict(
-    type='OptimWrapper',
-    optimizer=optimizer,
-    paramwise_cfg=dict(
-        custom_keys={
-            'ln': dict(decay_mult=0.0),
-            'bias': dict(decay_mult=0.0),
-            'pos_embed': dict(decay_mult=0.),
-            'mask_token': dict(decay_mult=0.),
-            'cls_token': dict(decay_mult=0.)
-        }))
-"""
 # optimizer
 optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optim_wrapper = dict(
@@ -29,25 +12,6 @@ optim_wrapper = dict(
             'mask_token': dict(decay_mult=0.),
             'cls_token': dict(decay_mult=0.)
         }))
-
-# learning rate scheduler
-param_scheduler = [
-    dict(
-        type='LinearLR',
-        start_factor=0.0001,
-        by_epoch=True,
-        begin=0,
-        end=10,
-        convert_to_iter_based=True),
-    dict(
-        type='CosineAnnealingLR',
-        T_max=260,
-        by_epoch=True,
-        begin=10,
-        end=20,
-        convert_to_iter_based=True)
-]
-
 
 
 # runtime settings
