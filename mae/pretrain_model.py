ab3c25e02693d7be65db2174c2cfa8e1d87b1265
Max Dietsch <diedietschies@gmail.com>
2023-10-03 08:50:30 +0200

    mae

--- mae/pretrain_model.py
+++ mae/pretrain_model.py
@@ -8,7 +8,7 @@ model = dict(
     backbone=dict(type='MAEViT', arch='s', img_size=(320, 320), patch_size=10, mask_ratio=0.75),
     neck=dict(
         type='MAEPretrainDecoder',
-        num_patches = 1296,
+        num_patches = 1024,
         patch_size=10,
         in_chans=3,
         embed_dim=768,
