afd2314cb08e3fa3cdbacb44834f84531c666093
Max Dietsch <diedietschies@gmail.com>
2023-10-03 14:35:57 +0200

    mae

--- mae/pretrain.py
+++ mae/pretrain.py
@@ -7,7 +7,7 @@ if torch.cuda.is_available():
     os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':16:8'
 
 cfg = Config.fromfile("./pretrain_configs.py")
-cfg.work_dir = './work_dirs/pretrain'
+cfg.work_dir = './work_dirs/pretrain007'
 
 runner = Runner.from_cfg(cfg)
 runner.train()
