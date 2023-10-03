c292c79fcfd62678857ea2618efa65b0e954d21e
Max Dietsch <diedietschies@gmail.com>
2023-10-03 08:49:46 +0200

    mae

--- mae/pretrain_model.py
+++ mae/pretrain_model.py
@@ -5,11 +5,11 @@ model = dict(
         mean=[7.18, 4.85, 3.47],
         std=[34.02, 22.52, 16.46],
         bgr_to_rgb=True),
-    backbone=dict(type='MAEViT', arch='s', img_size=(320, 320), patch_size=9, mask_ratio=0.75),
+    backbone=dict(type='MAEViT', arch='s', img_size=(320, 320), patch_size=10, mask_ratio=0.75),
     neck=dict(
         type='MAEPretrainDecoder',
         num_patches = 1296,
-        patch_size=9,
+        patch_size=10,
         in_chans=3,
         embed_dim=768,
         decoder_embed_dim=512,
@@ -20,7 +20,7 @@ model = dict(
     head=dict(
         type='MAEPretrainHead',
         norm_pix=True,
-        patch_size=9,
+        patch_size=10,
         loss=dict(type='MAEReconstructionLoss')),
     init_cfg=[
         dict(type='Xavier', distribution='uniform', layer='Linear'),
