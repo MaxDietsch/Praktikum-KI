5d0ebead8e7424b4a9ef27e9be42b1bf42c22da0
Max Dietsch <diedietschies@gmail.com>
2023-10-05 10:30:51 +0200

    mae

---
+++ run_tests_train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#python test.py "$1" "$2" 100%
+
+python test.py "$1" "$2" 50%
+
+python test.py "$1" "$2" 25%
+
+python test.py "$1" "$2" 5000
+
+python test.py "$1" "$2" 1000
+
+python test.py "$1" "$2" 100
+
+python test.py "$1" "$2" 10
