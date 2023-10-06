408a5a8be346ec1a662660df7c1e327e94b00166
Max Dietsch <diedietschies@gmail.com>
2023-10-06 21:13:28 +0200

    mae

--- simsiam/runtime.py
+++ simsiam/runtime.py
@@ -23,7 +23,7 @@ vis_backends = [dict(type='LocalVisBackend')]
 visualizer = dict(
     type='SelfSupVisualizer', vis_backends=vis_backends, name='visualizer')
 
-randomness = dict(seed=30, deterministic=True)
+randomness = dict(seed=35, deterministic=True)
 
 log_level = 'INFO'
 load_from = None
