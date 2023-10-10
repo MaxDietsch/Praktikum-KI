1133c3375ac54d4cab8133342dce10d11ebdc554
Max Dietsch <diedietschies@gmail.com>
2023-10-10 08:42:32 +0200

    advancements

--- relative-location/data.py
+++ relative-location/data.py
@@ -21,8 +21,8 @@ train_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        ann_file='meta/train25.0%.txt',
-        data_prefix='train25.0%',
+        ann_file='meta/train750.txt',
+        data_prefix='train750',
         pipeline=train_pipeline),
     sampler=dict(type='DefaultSampler', shuffle=True),
     collate_fn=dict(type='default_collate'),
@@ -44,7 +44,7 @@ val_dataloader = dict(
 )
 val_evaluator = dict(type='mmcls.Accuracy', topk=(1, 1))
 
-#val_dataloader = None
-#val_evaluator = None
+val_dataloader = None
+val_evaluator = None
 test_dataloader = val_dataloader
 test_evaluator = val_evaluator
--- tools_created/train_split.py
+++ tools_created/train_split.py
@@ -23,8 +23,8 @@ def create_split(directory, percentage):
     os.mkdir(directory + f'/train{str(percentage * 100)}%')
     """
 
-    if os.path.exists(directory + f'/train10'):
-        shutil.rmtree(directory + f'/train10')
+    if os.path.exists(directory + f'/train750'):
+        shutil.rmtree(directory + f'/train750')
     os.mkdir(directory + f'/train10')
 
 
@@ -35,20 +35,20 @@ def create_split(directory, percentage):
                 lines.append(line.strip())
     
     #train_file = os.path.join(directory, 'meta', f'train{str(percentage * 100)}%.txt')
-    train_file = os.path.join(directory, 'meta', f'train10.txt')
+    train_file = os.path.join(directory, 'meta', f'train750.txt')
 
 
     with open(train_file, "w") as trainfile:
         for i, line in tqdm.tqdm(enumerate(lines)):
             file, cls = line.split()
             #shutil.copy(os.path.join(directory, 'train', file), directory + f'/train{str(percentage * 100)}%')
-            shutil.copy(os.path.join(directory, 'train', file), directory + f'/train10')
+            shutil.copy(os.path.join(directory, 'train', file), directory + f'/train750')
             trainfile.write(line + '\n')
 
 
 if __name__ == "__main__":
     directory = "../data_dir/SUN"
-    percentage = 10/63490 #0.25
+    percentage = 750/63490 #0.25
     create_split(directory, percentage)
 
     x = check_ratio("../data_dir/SUN/meta/train10.txt", 10)
