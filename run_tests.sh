cd5bce41f38232c8adaf8d421c68c0d47b24f402
Max Dietsch <diedietschies@gmail.com>
2023-10-10 09:10:40 +0200

    advancements

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-python test.py "$1" "$2" 100%
+#python test.py "$1" "$2" 100%
 
-python test.py "$1" "$2" 50%
+#python test.py "$1" "$2" 50%
 
-python test.py "$1" "$2" 25%
+#python test.py "$1" "$2" 25%
 
-python test.py "$1" "$2" 5000
+#python test.py "$1" "$2" 5000
 
-python test.py "$1" "$2" 1000
+#python test.py "$1" "$2" 1000
 
-python test.py "$1" "$2" 100
+python test.py "$1" "$2" 750
 
-python test.py "$1" "$2" 10
+python test.py "$1" "$2" 500
+
+python test.py "$1" "$2" 250
+
+#python test.py "$1" "$2" 100
+
+#python test.py "$1" "$2" 10
