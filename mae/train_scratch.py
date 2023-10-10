1cb64cab69d7be1d847290ff49a9f61e3b281a9b
Max Dietsch <diedietschies@gmail.com>
2023-10-10 12:47:10 +0200

    advancements

--- mae/train_scratch.py
+++ mae/train_scratch.py
@@ -4,7 +4,7 @@ import torch
 import os
 
 cfg = Config.fromfile('configs.py')
-cfg.work_dir = './work_dirs/train_scratch/500'
+cfg.work_dir = './work_dirs/train_scratch/250'
 
 runner = Runner.from_cfg(cfg)
 runner.train()
