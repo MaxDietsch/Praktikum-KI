d0de730fca50abd6eeade3d1f36e2e432dd2d9ff
Max Dietsch <diedietschies@gmail.com>
2023-10-10 11:44:50 +0200

    advancements

--- mae/data.py
+++ mae/data.py
@@ -25,8 +25,8 @@ train_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        ann_file='meta/train10.txt',
-        data_prefix='train10',
+        ann_file='meta/train750.txt',
+        data_prefix='train750',
         pipeline=train_pipeline)
 )
 
@@ -48,8 +48,8 @@ val_dataloader = dict(
 
 val_evaluator = dict(type='mmcls.Accuracy', topk=(1, 1))
 
-#val_dataloader = None
-#val_evaluator = None
+val_dataloader = None
+val_evaluator = None
 
 test_dataloader = val_dataloader
 test_evaluator = val_evaluator
--- mae/train_scratch.py
+++ mae/train_scratch.py
@@ -4,7 +4,7 @@ import torch
 import os
 
 cfg = Config.fromfile('configs.py')
-cfg.work_dir = './work_dirs/train_scratch/10x'
+cfg.work_dir = './work_dirs/train_scratch/750'
 
 runner = Runner.from_cfg(cfg)
 runner.train()
